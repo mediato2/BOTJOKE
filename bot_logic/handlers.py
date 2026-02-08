b16532e00305e78aefa284cb0cedf0e62913fe38
unknown <luzin.alexander@example.com>
2026-02-08 15:40:42 +0300

    1 commit

---
+++ bot_logic/handlers.py
@@ -0,0 +1,17 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from bot_logic.ai_tool import get_gigachat_response
+
+handlers_router = Router()
+
+@handlers_router.message(Command(commands=["start"]))
+async def start_handler(message: Message):
+    await message.answer(text="ЙОу внучок? андерстенд, Я бот для йоу момент! Просто отправь мне свой текст...")
+
+@handlers_router.message()
+async def ai_handler(message: Message):
+    ai_response = await get_gigachat_response(user_text=message.text)
+
+    await message.answer(text = ai_response)
